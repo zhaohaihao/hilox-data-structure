d70adc4775b5963c107bd5e36b0d8849e3f12e7f
zhaohaihao <zhh2933@gmail.com>
2019-09-16 17:11:40 +0800

    :octocat: 队列对比测试

---
+++ 03-queue/src/main/java/com/zhh/queue/QueueTest.java
@@ -0,0 +1,48 @@
+package com.zhh.queue;
+
+import com.zhh.queue.impl.ArrayQueue;
+import com.zhh.queue.impl.LoopQueue;
+
+import java.util.Random;
+
+/**
+ * @author zhh
+ * @description 队列测试
+ * @date 2019-09-16 16:47
+ */
+public class QueueTest {
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        System.out.println(String.format("ArrayQueue spend %s s", queueTest(arrayQueue, opCount)));
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        System.out.println(String.format("LoopQueue spend %s s", queueTest(loopQueue, opCount)));
+    }
+
+    /**
+     * 测试使用指定队列运行opCount个入队和出队操作所花费的时间, 单位秒
+     *
+     * @param queue   指定队列
+     * @param opCount 操作数
+     * @return
+     */
+    public static double queueTest(Queue<Integer> queue, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+
+        for (int i = 0; i < opCount; i++) {
+            queue.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+}
